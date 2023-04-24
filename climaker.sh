36c4316a2c74a0abbb82929cfefb38bcaad7a7f0
CmdIglo <maxwell.leu@web.de>
2023-04-24 20:25:29 +0200

    First commit of climaker; not yet made for use

---
+++ climaker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This script will make any shell script from this repo a CLI
+#Note that the script has to be saved on the machine this script is running on
+
+#Filename of the script
+filename=$1
+
+cp $filename "$filename"
+
