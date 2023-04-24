313f69ce0e28bf77a6a23f64c6babeb1c4311d97
CmdIglo <maxwell.leu@web.de>
2023-04-24 21:13:51 +0200

    First implementation of script

--- dev.sh
+++ dev.sh
@@ -2,6 +2,8 @@
 
 #which project 
 mode=$1
+#projects dir
+directory=$2
 #-l flag for project listing; lists git projects available on the machine
 list_flag=''
 
@@ -10,4 +12,29 @@ print_usage() {
 	echo "Usage: dev [-l] <Projectname>" 1>&2
 }
 
-while getopts "l:"
+#is being executed, when the script runs into errors
+nostd_exit() {
+	print_usage
+	exit 1
+}
+
+#check if a flag is set
+while getopts "l" option; do
+	case "${option}" in
+		l)
+			ls $directory | echo
+			;;
+		*)
+			nostd_exit
+			;;
+	esac
+done
+
+#check if the project exists locally
+if [ -d $directory$mode] 
+then
+	cd $directory$mode
+else
+	echo "Project does not exist locally"
+	nostd_exit
+fi
